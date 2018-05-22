61aa5bc7d8f3e8176dc94ff9fca2e8b2e5e49113
Mykhailo418 <smmichael418@gmail.com>
2018-05-22 20:40:54 +0300

    predeploy

--- configs/default.js
+++ configs/default.js
@@ -2,17 +2,17 @@ module.exports = {
 	port: 8000,
 	secret: 'mysecret',
   	root: process.cwd(),
-  	mode: 'test_prod',
+  	mode: 'prod',
     url: 'http://localhost',
   	public: {
   		html:  `${process.cwd()}/public/html`
   	},
   	db: {
-  		host: 'mongodb://localhost/tic_tac_toe'
+  		host: 'mongodb://Michael418:1q2w3e4r5t6y@ds245687.mlab.com:45687/tic_tac_toe'
   	},
   	hash: {
 	  length:     128,
 	  // may be slow(!): iterations = 12000 take ~60ms to generate strong password
-	  iterations: this.mode == 'prod' ? 12000 : 1
+	  iterations: this.mode == 'production' ? 12000 : 1
 	}
 }
