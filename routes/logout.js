be5b6b6d0a956565310b594bd89b6a1d3d546028
Mykhailo418 <smmichael418@gmail.com>
2018-05-25 19:15:49 +0300

    Revert "Revert "comment docker""
    This reverts commit 98bf7aea216984d81ed8d41270b765723decfe05.

--- routes/logout.js
+++ routes/logout.js
@@ -3,12 +3,12 @@ const socket = require('../libs/socket');
 
 exports.get = async function(ctx, next){
 	// socket io logout semd message
-	if (ctx.session.socketIds) {
+	/*if (ctx.session.socketIds) {
 	    ctx.session.socketIds.forEach(function(socketId) {
 	      console.log("emit to", socketId);
 	      socket.emitter.to(socketId).emit('logout');
 	    });
-	 }
+	 }*/
 	await ctx.logout();
 	ctx.session = null;
 	ctx.redirect('/login');
