2351339ecebc1c570aa08df80a64c77598ed0716
Mykhailo418 <smmichael418@gmail.com>
2018-05-26 14:51:02 +0300

    add winston-mongodb

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,3 +1,5 @@
+const winston = require('winston');
+
 module.exports = app => app.use(async (ctx, next) => {
   try {
     await next();
@@ -6,11 +8,13 @@ module.exports = app => app.use(async (ctx, next) => {
       // could use template methods to render error page
       ctx.body = e.message;
       ctx.status = e.status;
+      winston.error(e);
     } else {
       let status = (typeof e == 'number') ? e : 500;
       ctx.body = 'Error ' + status;
       ctx.status = status;
       console.error('-- ERROR:',e.message, e.stack);
+      winston.error(e);
     }
 
   }
